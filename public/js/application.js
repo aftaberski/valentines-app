de1d4f43941f6f24786d7f84e3d52246b8720cf3
Anna Taberski <aftaberski@gmail.com>
2015-02-14 12:30:04 -0500

    add jquery to toggle login/signup

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $('.welcome-login').on("click", function(event){
+    event.preventDefault();
+    $('#login_form').toggle();
+  })
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+   $('.welcome-signup').on("click", function(event){
+    event.preventDefault();
+    $('#signup_form').toggle();
+  })
+
 });
